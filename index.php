6213453783c68ff77edbaade7bd8d9eb00107d64
SGTWaffles-F34 <gavinmsherman@gmail.com>
2023-03-09 15:43:36 -0500

    finished job app 2 requirements

--- index.php
+++ index.php
@@ -3,8 +3,16 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//OB-Start to get around header already sent error
+ob_start();
+
+
 //Start a session
 session_start();
+
+//Var dump for debugging
+var_dump($_SESSION);
+//var_dump($_POST);
 
 //Require the necessary files
 require_once('vendor/autoload.php');
@@ -54,7 +62,7 @@ $f3->route('GET|POST /personinfo', function($f3) {
 $f3->route('GET|POST /experience', function($f3) {
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
+        var_dump($_POST);
         $_SESSION['bio'] = $_POST['bio'];
         $_SESSION['ghub'] = $_POST['ghub'];
         $_SESSION['yrexp'] = $_POST['yrexp'];
@@ -72,8 +80,33 @@ $f3->route('GET|POST /experience', function($f3) {
 //Job Openings and Mailing List route
 $f3->route('GET|POST /jobsandmailing', function($f3) {
 
+    echo "<p> jobs and mailing </p>";
+    echo ($_SERVER['REQUEST_METHOD']);
 
-    //Instantiate a view
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        echo "<p>POST </p>";
+//        var_dump($_POST);
+
+        //Add development jobs to session array
+        $jobString = isset($_POST['dev']) ?
+            implode(", ",$_POST['dev']) : "";
+
+        $_SESSION['devJobs'] = $jobString;
+
+        //Add industry verticals to session array
+        $indString = isset($_POST['indV']) ?
+            implode(", ",$_POST['indV']) : "";
+
+        $_SESSION['indVerts'] = $indString;
+
+        //Reroute to summary page
+        $f3->reroute('summary');
+    }
+
+    $f3->set('devJobs', ['JavaScript', 'PHP', 'Java', 'Python', 'HTML', 'CSS', 'ReactJS', 'NodeJS']);
+    $f3->set('indVerts', ['Saas', 'Health Tech', 'Ag tech', 'HR Tech', 'Industrial Tech', 'Cybersecurity']);
+
+        //Instantiate a view
     $view = new Template();
     echo $view->render("views/jobsandmailing.html");
 });
@@ -83,8 +116,14 @@ $f3->route('GET|POST /summary', function() {
     //Instantiate a view
     $view = new Template();
     echo $view->render("views/summary.html");
+
+    session_destroy();
 });
 
 
 //run fat-free
 $f3->run();
+
+//
+ob_flush();
+
